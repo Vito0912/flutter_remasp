8e4d7aa9d38bf3dd431aff5b6c32dbc84544c080
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2024-12-19 21:27:49 +0100

    added acc highlighting

--- lib/providers/register_provider.dart
+++ lib/providers/register_provider.dart
@@ -4,10 +4,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class RegisterNotifier extends ChangeNotifier {
   final Map<int, BigInt> _registers = {};
+  int? lastModifiedRegister;
+
+  get lastModified => lastModifiedRegister;
+
+  void clearLastModified() {
+    lastModifiedRegister = null;
+  }
 
   void addValue(int key, BigInt value, {bool notify = true}) {
+    lastModifiedRegister = key;
     _registers[key] = value;
     if (notify) notifyListeners();
+    lastModifiedRegister = key;
   }
 
   void setValues(Map<int, BigInt> values) {
@@ -19,6 +28,7 @@ class RegisterNotifier extends ChangeNotifier {
   void removeValue(int key) {
     if (_registers.containsKey(key)) {
       _registers.remove(key);
+      lastModifiedRegister = key;
       notifyListeners();
     }
   }
@@ -26,23 +36,27 @@ class RegisterNotifier extends ChangeNotifier {
   void updateValue(int key, BigInt newValue, {bool notify = true}) {
     if (_registers.containsKey(key)) {
       _registers[key] = newValue;
+      lastModifiedRegister = key;
       if (notify) notifyListeners();
     }
   }
 
   BigInt getStarValue(int key) {
     if (getValue(key).isValidInt) {
+      lastModifiedRegister = getValue(key).toInt();
       return _registers[getValue(key).toInt()] ?? BigInt.zero;
     }
     throw Exception(S.current.errorRegisterOverflow);
   }
 
   BigInt getValue(int key) {
+    if (key != 0) lastModifiedRegister = key;
     return _registers[key] ?? BigInt.zero;
   }
 
   void clearValues({bool notify = true}) {
     _registers.clear();
+    lastModifiedRegister = null;
     if (notify) notifyListeners();
   }
 
--- lib/screens/modules/registers.dart
+++ lib/screens/modules/registers.dart
@@ -1,5 +1,7 @@
 import 'package:fluent_ui/fluent_ui.dart' hide Colors;
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_remasp/globals.dart';
 import 'package:flutter_remasp/providers/register_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -20,46 +22,71 @@ class Registers extends StatelessWidget {
           final TextEditingController controller =
               TextEditingController(text: value.toString());
 
-          return Container(
-            decoration: BoxDecoration(
-              color: index % 2 == 0
-                  ? theme.acrylicBackgroundColor
-                  : Color(0xFF383838),
-              // First register rounded corners
-              borderRadius: index == 0
-                  ? BorderRadius.only(
-                      topLeft: Radius.circular(4),
-                      topRight: Radius.circular(4),
-                    )
-                  : null,
-            ),
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 4.0, vertical: 1.0),
-              child: Row(
-                children: [
-                  index == 0 ? Text("Akk") : Text("$index"),
-                  SizedBox(width: 10),
-                  Expanded(
-                      child: SizedBox(
-                          height: 22,
-                          child: TextBox(
-                            controller: controller,
-                            style: TextStyle(fontSize: 12),
-                            keyboardType: TextInputType.number,
-                            inputFormatters: <TextInputFormatter>[
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                            padding: EdgeInsets.only(left: 4, top: 4, right: 2),
-                            onChanged: (value) =>
-                                updateValue(controller, ref, index),
-                            onTap: () => controller.selection = TextSelection(
-                                baseOffset: 0,
-                                extentOffset: controller.value.text.length),
-                          ))),
-                ],
+          return Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: index % 2 == 0
+                      ? theme.acrylicBackgroundColor
+                      : Color(0xFF383838),
+                  // First register rounded corners
+                  borderRadius: index == 0
+                      ? BorderRadius.only(
+                          topLeft: Radius.circular(4),
+                          topRight: Radius.circular(4),
+                        )
+                      : null,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 4.0, vertical: 1.0),
+                  child: Row(
+                    children: [
+                      index == 0 ? Text("Akk") : Text("$index"),
+                      SizedBox(width: 10),
+                      Expanded(
+                          child: SizedBox(
+                              height: 22,
+                              child: TextBox(
+                                controller: controller,
+                                style: TextStyle(fontSize: 12),
+                                keyboardType: TextInputType.number,
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                padding:
+                                    EdgeInsets.only(left: 4, top: 4, right: 2),
+                                onChanged: (value) =>
+                                    updateValue(controller, ref, index),
+                                onTap: () => controller.selection =
+                                    TextSelection(
+                                        baseOffset: 0,
+                                        extentOffset:
+                                            controller.value.text.length),
+                              ))),
+                    ],
+                  ),
+                ),
               ),
-            ),
+              if (index == values.lastModified)
+                FutureBuilder(
+                    future:
+                        Future.delayed(Duration(milliseconds: 0), () => false),
+                    initialData: true,
+                    builder: (context, snapshot) {
+                      final bool isVisible = snapshot.data ?? true;
+
+                      return AnimatedOpacity(
+                        duration: Duration(
+                            milliseconds: (speed.inMilliseconds * 1.5).toInt()),
+                        opacity: isVisible ? 1 : 0,
+                        child: Container(
+                          height: 24,
+                          color: Colors.yellowAccent.withValues(alpha: 0.2),
+                        ),
+                      );
+                    }),
+            ],
           );
         });
       },
